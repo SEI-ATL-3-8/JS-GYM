872cfc355daec0a163bab0ac7f757f8dbc4eb855
Ryan Lugtu <ryanlugtu@gmail.com>
2021-03-15 09:57:49 -0400

    added comment to birthday

--- problems/birthday.js
+++ problems/birthday.js
@@ -1,20 +1,10 @@
 /* Given this object, write a function that takes in a name and returns the string "hi my name is [name] and my birthday is [birthday]".
 If the name does not exist then return a string "this person does not exist "
+ex input : myBirthdayFunc("bob")
+ex output : "hi my name is bob and my birthday is June 8"
 
-myFriends = {
-    "bob": {
-        age: 24,
-        birthday: "June 8"
-    },
-    "paul": {
-        age: 35,
-        birthday: "December 3"
-    },
-    "george": {
-        age: 28,
-        birthday: "August 16"
-    }
-}
+ex input: "myBirthdayFunc("ryan")
+ex output: this person does not exist 
 */
 let myFriends = {
     "bob": {
